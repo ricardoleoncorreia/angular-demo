24e088f92e04747e16a98a05252efa6adec97295
Ricardo Leon <ricardoleoncorreia@gmail.com>
2018-10-14 18:34:46 -0400

    Include new components in routing module

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -9,6 +9,9 @@ import { AuthGuard } from './auth/auth.guard';
 import { SuccessComponent } from './components/success/success.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { AngularMaterialComponentsComponent } from './components/angular-material-components/angular-material-components.component';
+import { AngularMaterialTablesComponent } from './components/angular-material-tables/angular-material-tables.component';
+import { HighchartsComponent } from './components/highcharts/highcharts.component';
 
 const routes: Routes = [
   // If no route, redirect to /principal
@@ -16,6 +19,9 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent, },
   { path: 'login', component: LoginComponent, },
   { path: 'success', component: SuccessComponent, canActivate: [AuthGuard] },
+  { path: 'angular-material-components', component: AngularMaterialComponentsComponent },
+  { path: 'angular-material-tables', component: AngularMaterialTablesComponent },
+  { path: 'charts', component: HighchartsComponent },
   { path: '**', redirectTo: '/home' }
 ];
 
